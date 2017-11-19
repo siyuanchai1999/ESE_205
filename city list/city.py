faebd0b091fe7dca4155a5d77a7d22cd8fb307f5
siyuanchai1999 <31422075+siyuanchai1999@users.noreply.github.com>
2017-11-19 15:30:06 -0600

    Add files via upload

--- city list/city.py
+++ city list/city.py
@@ -1,19 +1,16 @@
 from urllib.request import urlopen
 import json
-import requests
 import csv
 
 file = urlopen('https://www.wunderground.com/about/faq/US_cities.asp')
 
-page = requests.get('https://www.wunderground.com/about/faq/US_cities.asp')
-txt = page.text
+txt = file.read().decode()
 first_point = txt.index('Central')
 end_point = txt.index('Peipeinimaru')
 new_txt = txt[first_point:end_point]
 
 txt_list = list(new_txt)
 txt_list.insert(0,'\n')
-length = txt_list.count('\n')
 dict_city = {}
 
 def get_str(start, end):
@@ -29,6 +26,10 @@ init_index = 0
 entries = txt_list.count('\n')
 for i in range(entries-1):
         space_index = txt_list.index(' ')
+        while txt_list[space_index+1] !=  ' ':
+            #print(space_index)
+            temp = txt_list[space_index + 1:]
+            space_index = temp.index(' ')+space_index+1
         city = get_str(init_index+1,space_index)
         state = get_str(init_index+31,init_index+33)
         dict_city[i] =  state + '/' + city
