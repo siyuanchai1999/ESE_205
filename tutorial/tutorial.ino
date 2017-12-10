88e6caefea77a0f8656c6b956adf21b0184de9c2
siyuanchai1999 <31422075+siyuanchai1999@users.noreply.github.com>
2017-12-10 17:37:05 -0600

    Update tutorial.ino

--- tutorial/tutorial.ino
+++ tutorial/tutorial.ino
@@ -4,11 +4,11 @@ void setup(){
 }
 
 void loop(){
-  /*Serial.println("Hello World!");
+  /*Serial.println("Hello World!"); From Arduino to RPi 
   delay(2000);*/
-  if(Serial.available()){
-    r = r * (Serial.read() - '0');
+  /*if(Serial.available()){         From RPi to Arduino
+    r = r * (Serial.read() - '0');  
     Serial.println(r);
-  }
+  }*/ 
 }
 
